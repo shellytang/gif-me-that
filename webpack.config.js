90ccd51a872033d3a3b30f7ec8387abc88cbec33
Shelly Tang <shellytang@gmail.com>
2017-09-23 19:34:35 -0700

    project scaffold

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('dotenv').config({path: `${__dirname}/.env`});
+const ExtractPlugin = require('extract-text-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    filename: 'bundle-[hash].js',
+    path: `${__dirname}/build`,
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlPlugin({ template: `${__dirname}/src/index.html` }),
+    new ExtractPlugin('blundle-[hash].css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract(['css-loader','sass-loader']),
+      },
+    ],
+  },
+};
