18ed4a52788e2ae34888cdba4bdea9be3ef409f4
Shelly Tang <shellytang@gmail.com>
2017-09-23 21:39:01 -0700

    initial files

---
+++ src/component/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://api.giphy.com/v1/gifs/';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <main>
+        <h1>gif machine</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
