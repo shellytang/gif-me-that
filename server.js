600d0400d238e91068f6455beee1092fb2a8b982
Shelly Tang <shellytang@gmail.com>
2017-09-26 22:44:27 -0700

    removed fs readfile

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(`${__dirname}/build`))
-app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
+app.use(express.static(`${__dirname}/build`));
 
 const PORT = process.env.PORT || 3000;
 
