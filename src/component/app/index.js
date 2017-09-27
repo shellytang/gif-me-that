c74ad25aa914184392b333827c8cdffc91f46873
Shelly Tang <shellytang@gmail.com>
2017-09-26 19:40:51 -0700

    removed use strict

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from 'react';
 import axios from 'axios';
 import SearchBar from '../search-bar';
--- src/component/gif-list-item/index.js
+++ src/component/gif-list-item/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from 'react';
 
 class GifListItem extends React.Component {
